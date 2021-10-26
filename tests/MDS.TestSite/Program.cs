da740dbe0859f4614866bc73e228b353004f0554
The Sharp Ninja <ninja@thesharp.ninja>
2021-10-25 23:39:23 -0500

    Update Program.cs

--- tests/MDS.TestSite/Program.cs
+++ tests/MDS.TestSite/Program.cs
@@ -4,18 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddMarkdownServer();
 
 builder.Services.AddHttpContextAccessor();
-//builder.Services.AddOptions();
 
 var app = builder.Build();
 
 app.UseMarkdownServer();
 
 app.UseStaticFiles();
-
-//app.MapGet("/", async context 
-//    => await MarkdownServerOptions
-//        .Current
-//        .MarkdownFileExecute(context)
-//        .ExecuteAsync(context));
 
 app.Run();
